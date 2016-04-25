655d98ad69bce50654ba1d043512dc51d528ee13
FamaLJ <blongjohn2017@kellogg.northwestern.edu>
2016-04-25 18:13:50 -0500

    update

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -16,7 +16,7 @@ class GeocodingController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
-
+url="http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
 parsed_data = JSON.parse(open(url).read)
 
     @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
