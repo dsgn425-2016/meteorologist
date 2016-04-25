32590ddf63784f854de194e593d7f0602d67154e
axepolzin <apolzin2017@users.noreply.github.com>
2016-04-25 17:17:07 -0500

    axepolzin-finished

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -8,7 +8,7 @@ class MeteorologistController < ApplicationController
 
   def street_to_weather
     @street_address = params[:user_street_address]
-    url_safe_street_address = URI.encode(@street_address)
+    @url_safe_street_address = URI.encode(@street_address)
 
     # ==========================================================================
     # Your code goes below.
@@ -16,18 +16,31 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+    require 'open-uri'
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_street_address}"
+
+    require 'json'
+        parsed_data = JSON.parse(open(url).read)
+        @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+        @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+    require 'open-uri'
+        url = "https://api.forecast.io/forecast/d387b8fb36b2aefdf789af0335e401e0/#{@latitude},#{@longitude}"
+
+    require 'json'
+        parsed_data = JSON.parse(open(url).read)
+        @current_temperature = parsed_data["currently"]["temperature"]
+
+        @current_summary = parsed_data["currently"]["summary"]
+
+        @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
+
+        @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+
+        @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
 
-
-    @current_temperature = "Replace this string with your answer."
-
-    @current_summary = "Replace this string with your answer."
-
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+  
 
     render("street_to_weather.html.erb")
   end
