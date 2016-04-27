ac1238fe7096be122b1388c5f8059568a2631970
jillseidel <jseidel2017@users.noreply.github.com>
2016-04-27 14:05:07 -0500

    first two pages complete

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -15,18 +15,29 @@ class ForecastController < ApplicationController
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+lat = @lat
+lng = @lng
+
+require 'json'
+url = "https://api.forecast.io/forecast/8ecb5c3c292fb07c022c5f685dc1b6b7/" + lat + "," + lng
+parsed_data = JSON.parse(open(url).read)
+
+temp = parsed_data["currently"]["temperature"]
+summary_current = parsed_data["currently"]["summary"]
+summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
+summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+summary_of_next_several_days = parsed_data["daily"]["summary"]
 
 
+    @current_temperature = temp
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = summary_current
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = summary_of_next_sixty_minutes
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_several_hours = summary_of_next_several_hours
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = summary_of_next_several_days
 
     render("coords_to_weather.html.erb")
   end
