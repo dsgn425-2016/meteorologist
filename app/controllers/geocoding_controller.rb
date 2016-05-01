84f8ef3eafc66e5e38ca23a4eaa8ab7bc9a54070
Anyadigann <adigann2017@kellogg.northwestern.edu>
2016-05-01 14:29:58 -0500

    Meteorologist_Anya DiGann

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -8,7 +8,7 @@ class GeocodingController < ApplicationController
 
   def street_to_coords
     @street_address = params[:user_street_address]
-    url_safe_street_address = URI.encode(@street_address)
+    url_safe_street_address = URI.encode(@street_address) #turns input address into the 1600+Street+Mountain View... format for the url
 
     # ==========================================================================
     # Your code goes below.
@@ -17,11 +17,20 @@ class GeocodingController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    require 'open-uri'
 
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_street_address #append the input address into the url
 
-    @latitude = "Replace this string with your answer."
+    raw_data = open(url).read #puts the url API data into the raw_data variable
 
-    @longitude = "Replace this string with your answer."
+    require 'json' #pulls the json language bot-thingy for ruby
+    JSON.parse(raw_data) #JSON class parse the raw data
+    parsed_data = JSON.parse(raw_data) #parsed_data.keys will show you the keys in the API hash
+    results = parsed_data["results"] 
+
+    @latitude = results[0]["geometry"]["location"]["lat"]
+
+    @longitude = results[0]["geometry"]["location"]["lng"]
 
     render("street_to_coords.html.erb")
   end
