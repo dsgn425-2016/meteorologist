8208eb317aac678561201a0f3588d61e91f641c4
elaineichen <elainechen2017@kellogg.northwestern.edu>
2016-05-01 16:09:54 -0500

    forecast changes

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,7 +16,11 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+url = "https://api.forecast.io/forecast/0995404f7323cd00ed4747b1754b0009/"+@lat._s+","+@lng._s
 
+raw_data = open(url).read
+
+parsed_data = JSON.parse(raw_data)
 
     @current_temperature = "Replace this string with your answer."
 
